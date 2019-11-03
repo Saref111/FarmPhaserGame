5468390418e8c5505c075fbcf15600c82164f23b
Saref111 <saref012@gmail.com>
2019-11-03 21:36:29 +0800

    Change resolution, preload sprites, creat chicken sprite

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
-var game = new Phaser.Game(640, 380, Phaser.AUTO);
+var game = new Phaser.Game(600, 300, Phaser.AUTO);
 
 var GameState = {
     preload: function(){
         this.load.image('background', 'assets/img/bc.png');
-        this.load.image();
-        this.load.image();
-        this.load.image();
-        this.load.image();    
+        this.load.image('pig', 'assets/img/pig.png');
+        this.load.image('cow', 'assets/img/cow.png');
+        this.load.image('sheep', 'assets/img/sheep.png');
+        this.load.image('chicken', 'assets/img/chicken.png');    
     },
 
     create: function(){
-        this.background = this.game.add.sprite(0, 0, 'background');
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.pageAlignVertically = true;
+        this.scale.pageAlignHorizontally = true;
+
+        this.background = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'background');
+        this.background.anchor.setTo(0.5);
+        this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
+        this.chicken.anchor.setTo(0.5);
+        this.chicken.scale.setTo(0.5);
     },
 
     update: function(){

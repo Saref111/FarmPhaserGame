f430ada4f7ea5455bb09253e892fdddc2e532ba3
Saref111 <saref012@gmail.com>
2019-11-03 20:48:09 +0800

    Create a game

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var game = new Phaser.Game(640, 380, Phaser.AUTO);
+
+var GameState = {
+    preload: function(){
+
+    },
+
+    create: function(){
+
+    },
+
+    update: function(){
+
+    }
+};
+
+game.state.add('GameState', GameState);
+game.state.start('GameState');
